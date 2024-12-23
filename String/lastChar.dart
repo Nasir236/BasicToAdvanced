ff5a34ded116fbb06593beb5677aba52d0f2e24a
NasirHussain <nasirzaib8587@gmail.com>
2024-12-23 12:54:25 -0800

    basic to advanced

---
+++ String/lastChar.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Findind Last Character of String
+  var str = "Hello Nasir";
+  var lastChar = str[str.length - 1];
+  print("Your LastChar String is: $lastChar");
+
+  // Second Example
+  var str1 = "Why Do You Scared";
+  var lastChar1 = str1[str1.length - 1];
+  print("Your LastChar String is: $lastChar1");
+}
---
+++ String/numberofString.dart
@@ -0,0 +1,23 @@
+void main() {
+  //substring      // take start range and end range index number
+  //sublist        // take start list and end list index number
+
+  // Substring method take it range
+  var str = "My Frind Zakir Shah is cute";
+  var subRange = str.substring(0, 14);
+  print("Your array indexing is: $subRange");
+  // second example sublist method
+  var number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+  var subNum = number.sublist(0, 6);
+  print("Your List Number is: $subNum");
+
+  // get last char and int
+  // Substring method take it range
+  var str2 = "My Frind Zakir Shah is cute";
+  var subRange2 = str2.substring(str2.length - 9);
+  print("Your Last array indexing is: $subRange2");
+  // second example sublist method
+  var number2 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+  var subNum2 = number2.sublist(number2.length - 3);
+  print("Your Last List Number is: $subNum2");
+}
