7f691131c9b3979ef99ec51aefceac03033892cc
NasirHussain <nasirzaib8587@gmail.com>
2025-01-24 12:03:07 -0800

    Polymorpisams concept

---
+++ polymorpisams.dart
@@ -0,0 +1,33 @@
+void main() {
+  // polymorpisam have two Types
+  // 1.OverLoading  never in dart language
+  // 2.OverRiding we use to overrifing method
+  // create instance
+  Student obj =
+      Student(); // Student class is Child but access to Parent Attributes
+  obj.name = "Hussain";
+  obj.age = 23;
+  obj.displayInfo();
+}
+
+class Person {
+  // create class
+  String? name;
+  int? age;
+
+  displayInfo() {
+    print('Name: $name');
+    print('Age: $age');
+  }
+}
+
+class Student extends Person {
+// Overriding Method
+  @override
+  displayInfo() {
+    print("Student OverRide Hugai");
+    super.displayInfo();
+  }
+}
+
+class Teacher {}
